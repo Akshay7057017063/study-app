e09cf79be443ca2ccb73e77cf03df6be7ecbe77a
Akshay7057017063 <114216378+Akshay7057017063@users.noreply.github.com>
2026-09-25 00:56:11 -0700

    Add course update and delete routes

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -3,11 +3,15 @@ const express = require("express");
 const {
   createCourse,
   getCourses,
+  updateCourse,
+  deleteCourse,
 } = require("../controllers/courseController");
 
 const router = express.Router();
 
 router.get("/", getCourses);
 router.post("/", createCourse);
+router.put("/:id", updateCourse);
+router.delete("/:id", deleteCourse);
 
 module.exports = router;
