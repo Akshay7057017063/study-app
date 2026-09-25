d699990aa905c2228c21dfff39b21fbff6f8f6b4
Akshay7057017063 <114216378+Akshay7057017063@users.noreply.github.com>
2026-09-25 00:03:53 -0700

    Add faculty update and delete routes

--- backend/routes/facultyRoutes.js
+++ backend/routes/facultyRoutes.js
@@ -3,11 +3,15 @@ const express = require("express");
 const {
   createFaculty,
   getFaculties,
+  updateFaculty,
+  deleteFaculty,
 } = require("../controllers/facultyController");
 
 const router = express.Router();
 
 router.get("/", getFaculties);
 router.post("/", createFaculty);
+router.put("/:id", updateFaculty);
+router.delete("/:id", deleteFaculty);
 
 module.exports = router;
